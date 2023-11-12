64c195a8ebc0df390e0390ecce6057e03bd8a19a
michael <mvanderlyn27@gmail.com>
2023-11-11 23:31:56 -0500

    switching to route handlers

--- types/photospotTypes.ts
+++ types/photospotTypes.ts
@@ -5,3 +5,18 @@ export interface Photolist {
     id: number;
     name: string;
 }
+export interface PhotolistInput {
+    created_at?: string; 
+    created_by?: string;
+    description?: string;
+    id: number;
+    name: string;
+}
+
+export interface Photospot {
+    created_at?: string;
+    created_by?: string;
+    description?: string;
+    id: number;
+    name?: string;
+}
