77054c59669a9492779a1779c099aea81d0b294d
michael <mvanderlyn27@gmail.com>
2024-05-08 17:18:10 -0400

    pushing mby broken preview map

--- app/photospot/[id]/page.tsx
+++ app/photospot/[id]/page.tsx
@@ -1,31 +1,30 @@
-"use client"
-import { getById } from "@/app/serverActions/photospots/getById"
-import PhotospotGrid from "@/components/PhotoSpotGrid"
-import PreviewMap from "@/components/maps/previewMap"
-import PhotospotInfo from "@/components/photospot/photospotInfo"
-import { Photospot } from "@/types/photospotTypes"
-import { useEffect, useState } from "react"
+"use client";
+import { getById } from "@/app/serverActions/photospots/getById";
+import PhotospotGrid from "@/components/PhotoSpotGrid";
+import PreviewMap from "@/components/maps/previewMap";
+import PhotospotInfo from "@/components/photospot/photospotInfo";
+import { Photospot } from "@/types/photospotTypes";
+import { useEffect, useState } from "react";
 
 export default function PhotospotPage({ params }: { params: { id: string } }) {
-    const [photospotData, setPhotoSpotData] = useState<Photospot | null>(null)
-    useEffect(() => {
-        //pull info from photospot based on id 
-        getById(parseInt(params.id)).then((photospot: Photospot) => {
-            setPhotoSpotData(photospot)
-        })
-    }, [params.id])
-    return (
-        <div className="flex flex-col w-full justify-center gap-8">
-            <div className="flex flex-row w-full gap-8">
-                <div className="flex-1">
-                    <PhotospotInfo photospot={photospotData} />
-                </div>
-                <div className="flex-1">
-                    <PreviewMap />
-                </div>
-
-            </div>
-            <PhotospotGrid photospots={[]} />
+  const [photospotData, setPhotoSpotData] = useState<Photospot | null>(null);
+  useEffect(() => {
+    //pull info from photospot based on id
+    getById(parseInt(params.id)).then((photospot: Photospot) => {
+      setPhotoSpotData(photospot);
+    });
+  }, [params.id]);
+  return (
+    <div className="flex flex-col w-full justify-center gap-8">
+      <div className="flex flex-row w-full gap-8">
+        <div className="flex-1">
+          <PhotospotInfo photospot={photospotData} />
         </div>
-    )
+        <div className="flex-1">
+          <PreviewMap lat={photospotData?.lat} lng={photospotData?.lng} />
+        </div>
+      </div>
+      <PhotospotGrid photospots={[]} />
+    </div>
+  );
 }
--- components/maps/previewMap.tsx
+++ components/maps/previewMap.tsx
@@ -1,7 +1,36 @@
-export default function PreviewMap() {
-    return (
-        <div>
-            Preview Map
-        </div>
-    )
+"use client";
+import { useRef } from "react";
+import { Map as MapboxMap, Marker } from "react-map-gl";
+
+export default function PreviewMap({
+  lat,
+  lng,
+}: {
+  lat: number | undefined;
+  lng: number | undefined;
+}) {
+  const mapRef = useRef<any>(null);
+  const mapBoxToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+    ? process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+    : "";
+  return (
+    <MapboxMap
+      initialViewState={{
+        longitude: lng,
+        latitude: lat,
+        zoom: 13,
+      }}
+      // reuseMaps={true}
+      mapStyle="mapbox://styles/mvanderlyn27/clc8gyohu000114pl9hy6zzdt"
+      mapboxAccessToken={mapBoxToken}
+      ref={mapRef}
+      cursor="auto"
+    >
+      {lat && lng && (
+        <Marker longitude={lng} latitude={lat} anchor="bottom">
+          <img className="w-10 h-10" src="/selectedPin.svg" />
+        </Marker>
+      )}
+    </MapboxMap>
+  );
 }
