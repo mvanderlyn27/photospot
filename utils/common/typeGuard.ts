3b3fa2d717468f6cd7b67fbc95b6b21692d54706
michael <mvanderlyn27@gmail.com>
2024-07-04 19:28:19 -0400

    fixed another bug

--- utils/common/typeGuard.ts
+++ utils/common/typeGuard.ts
@@ -4,7 +4,7 @@ export const isPhotospot = (input: any): input is Photospot => {
     return "id" in input && "location_name" in input;
 }
 export const isPhotoshot = (input: any): input is Photoshot => {
-    return "id" in input && "photospot_id" in input;
+    return "id" in input && "name" in input;
 }
 export const isProfile = (input: any): input is Profile => {
     return "id" in input && "username" in input && "bio" in input;
