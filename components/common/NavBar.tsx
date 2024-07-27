cc6d625adbf7df0ab92b8919c8e5d6d3630bdddd
michael <mvanderlyn27@gmail.com>
2024-07-27 16:02:18 -0400

    added hamburger for mobile

--- components/common/NavBar.tsx
+++ components/common/NavBar.tsx
@@ -7,10 +7,24 @@ import useSWR from "swr";
 import { fetcher } from "@/utils/common/fetcher";
 import { useEffect } from "react";
 import { useTheme } from "next-themes";
-
+import { useBreakpoint } from "@/hooks/tailwind";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { IoMenuOutline } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { IoHome } from "react-icons/io5";
+import { FaMapMarkedAlt } from "react-icons/fa";
+import { MdAddAPhoto } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
 export default function NavBar() {
   const { setTheme } = useTheme();
   //get user logged in, render navbar
+  const small = useBreakpoint("sm");
   const {
     data: user,
     isLoading: userLoading,
@@ -25,12 +39,12 @@ export default function NavBar() {
   const pathStart = pathname.split("/")[1];
   return (
     <nav className="w-full flex justify-center h-16">
-      <div className="w-full  flex justify-between items-center p-3 pl-10 pr-10 text-foreground">
+      <div className="w-full  flex justify-between items-center p-3 lg:pl-10 lg:pr-10 text-foreground">
         <Link href={user ? "/home" : "/"}>
           <h3 className="text-3xl font-semibold ">PhotoSpot</h3>
         </Link>
 
-        {user && (
+        {user && !small && (
           <div className="flex items-center gap-4">
             Hey, <b>{user.username ? user.username : user.email}!</b>
             <div className="flex jusitfy-between items-center flex-row">
@@ -38,7 +52,7 @@ export default function NavBar() {
                 href="/home"
                 className={`${
                   pathStart === "home" ? "font-bold" : ""
-                } py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover`}
+                } cursor-pointer py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover`}
               >
                 {" "}
                 Home{" "}
@@ -47,7 +61,7 @@ export default function NavBar() {
                 href="/explore"
                 className={`${
                   pathStart === "explore" ? "font-bold" : ""
-                } py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover`}
+                } cursor-pointer py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover`}
               >
                 {" "}
                 Explore
@@ -56,7 +70,7 @@ export default function NavBar() {
                 href="/create"
                 className={`${
                   pathStart === "create" ? "font-bold" : ""
-                } py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover`}
+                } cursor-pointer py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover`}
               >
                 {" "}
                 Create
@@ -65,7 +79,7 @@ export default function NavBar() {
                 href="/user"
                 className={`${
                   pathStart === "user" ? "font-bold" : ""
-                } py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover`}
+                } cursor-pointer py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover`}
               >
                 {" "}
                 Users{" "}
@@ -74,7 +88,7 @@ export default function NavBar() {
                 href="/profile"
                 className={`${
                   pathStart === "profile" ? "font-bold" : ""
-                } py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover`}
+                } cursor-pointer py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover`}
               >
                 {" "}
                 Profile
@@ -91,6 +105,78 @@ export default function NavBar() {
             >
               <Button>Login</Button>
             </Link>
+          </div>
+        )}
+        {user && small && (
+          <div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline">
+                  <IoMenuOutline className="h-6 w-6" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56 color">
+                <Link
+                  href="/home"
+                  className={`${
+                    pathStart === "home" ? "font-bold" : ""
+                  } cursor-pointer py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover`}
+                >
+                  <DropdownMenuItem>
+                    <IoHome className="mr-2 h-6 w-6" />
+                    <span className="text-lg">Home</span>
+                  </DropdownMenuItem>
+                </Link>
+                <DropdownMenuSeparator />
+                <Link
+                  href="/explore"
+                  className={`${
+                    pathStart === "explore" ? "font-bold" : ""
+                  } cursor-pointer py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover`}
+                >
+                  <DropdownMenuItem>
+                    <FaMapMarkedAlt className="mr-2 h-6 w-6" />
+                    <span className="text-lg">Explore</span>
+                  </DropdownMenuItem>
+                </Link>
+                <DropdownMenuSeparator />
+                <Link
+                  href="/create"
+                  className={`${
+                    pathStart === "create" ? "font-bold" : ""
+                  } cursor-pointer py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover`}
+                >
+                  <DropdownMenuItem>
+                    <MdAddAPhoto className="mr-2 h-6 w-6" />
+                    <span className="text-lg">Create</span>
+                  </DropdownMenuItem>
+                </Link>
+                <DropdownMenuSeparator />
+                <Link
+                  href="/user"
+                  className={`${
+                    pathStart === "user" ? "font-bold" : ""
+                  } cursor-pointer py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover`}
+                >
+                  <DropdownMenuItem>
+                    <FaUsers className="mr-2 h-6 w-6" />
+                    <span className="text-lg">Users</span>
+                  </DropdownMenuItem>
+                </Link>
+                <DropdownMenuSeparator />
+                <Link
+                  href="/profile"
+                  className={`${
+                    pathStart === "profile" ? "font-bold" : ""
+                  } cursor-pointer py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover`}
+                >
+                  <DropdownMenuItem>
+                    <CgProfile className="mr-2 h-6 w-6" />
+                    <span className="text-lg">Profile</span>
+                  </DropdownMenuItem>
+                </Link>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         )}
       </div>
