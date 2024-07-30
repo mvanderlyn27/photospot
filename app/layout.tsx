4046c0a545f4760bceda6bb5ef88e4c666ed217c
michael <mvanderlyn27@gmail.com>
2024-07-30 17:18:49 -0400

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,12 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </head>
       <body
         className={cn(
           "h-screen bg-background font-sans antialiased",
