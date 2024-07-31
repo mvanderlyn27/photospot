31641da84a79445ac6538beb609118a0c28a9b4c
michael <mvanderlyn27@gmail.com>
2024-07-31 19:53:17 -0400

    build fix

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,10 @@
 import ProfilePageSection from "@/components/profilePage/profilePageSection";
+import { Suspense } from "react";
 
 export default function ProfilePage() {
-  return <ProfilePageSection />;
+  return (
+    <Suspense>
+      <ProfilePageSection />;
+    </Suspense>
+  );
 }
